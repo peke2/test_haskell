207b3e7a5c4929a962307349234a045f3d22235a
peke2 <abem@dc4.so-net.ne.jp>
2015-10-12 15:17:50 +0900

    ガードの意味がわかったので、後で見て分かるようコメントを追加。

--- guard/guard.hs
+++ guard/guard.hs
@@ -1,12 +1,22 @@
 import System.IO
 
+-- 【ガード】
+-- 引数の値で場合分けする場合に使う
+-- 引数が構造で異なる場合は以下の「パターン」を使用する
+--
+func::[Int]->Int
+func [] = 0
+func (x:_) = x
+
+
+-- ガード
 callHand :: Int -> String
 callHand h
   | h == 1 = "One"
   | h == 2 = "Two"
   | h == 3 = "Three"
-
+  | otherwise = "*other*"
 
 main = do
   line <- getLine
-  print(callHand read line::Int)
+  putStr(callHand (read line::Int))
